4b833e50205fa93847d0050cd709e540ececcca8
MUHAMMADZARYABKHAN-21 <muhammadzaryabkhan21@gmail.com>
2026-07-01 11:43:14 +0500

    revised the typos

--- practice3.py
+++ practice3.py
@@ -1,37 +1,19 @@
 import numpy as np
 
+# Example array
+arr2 = np.array([10, 20, 30, 40, 50, 60, 70])
 
-x= np.array([23,45,65,1,6,7,8])
-print(x)
-print(x[0])
-print(x[1])
-print(x[2])
-print(x[3])
-print(x[4])
-print(x[5])
-print(x[6])
+# Print slice from index 2 to 6 (exclusive of 6)
+print(arr2[2:6])   # → [30 40 50 60]
 
-print(x[-1])
-print(x[-2])
-print(x[-3])
-print(x[-4])
-print(x[-5])
-print(x[-6])
+# Another array
+arr3 = np.array([100, 200, 300, 400, 500, 600, 700, 800, 900, 1000])
 
+# Reverse with step -3 and multiply by 3
+print(arr3[::-3] * 3)   # → [3000 2100 1200 300]
 
+# Corrected array creation (typo fixed: np.aray → np.array)
+arr3 = np.array([1,2,3,4,5,6,7,8,9,10])
 
-arr = np.array([10,20,30,40,50])
-
-print(arr[1:4])
-
-
-arr2=np.array([10,20,30,40,50,60,70,80])
-print(arr2[2:6])
-
-
-
-
-
-
-
-
+print(arr3)        # Full array
+print(arr3[1:5])   # Slice from index 1 to 4 → [2 3 4 5]
