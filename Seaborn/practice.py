fe7ca5f41f2d109d09e86606e18a45b0934bb6cb
MUHAMMADZARYABKHAN-21 <muhammadzaryabkhan21@gmail.com>
2026-08-06 23:54:25 +0500

    more additions

--- Seaborn/practice.py
+++ Seaborn/practice.py
@@ -1,25 +1,69 @@
+# # # # import pandas as pd
+# # # # import seaborn as sns
+# # # # import matplotlib.pyplot as plt
+# # # # df=pd.read_csv("Titanic1.csv")
+# # # # sns.histplot(
+# # # #     data=df,
+# # # #     x="age",
+# # # #     kde=True,
+# # # # )
+# # # # plt.show()
+# # # import pandas as pd
+# # # import seaborn as sns
+# # # import matplotlib.pyplot as plt
+# # #
+# # # df = pd.read_csv("Titanic1.csv")
+# # #
+# # # sns.displot(data=df,x="age")
+# # # plt.show()
+# #
 # # import pandas as pd
 # # import seaborn as sns
 # # import matplotlib.pyplot as plt
 # # df=pd.read_csv("Titanic1.csv")
-# # sns.histplot(
+# # # sns.displot(
+# # #     data=df,
+# # #     x="age",
+# # #     kde=True,
+# # #     color="blue",
+# # #     col="sex"
+# # # )
+# # sns.displot(
 # #     data=df,
 # #     x="age",
-# #     kde=True,
+# #     color="green",
+# #     col="sex"
 # # )
+# # plt.title("Fare distribution")
+# #
 # # plt.show()
+#
 # import pandas as pd
 # import seaborn as sns
 # import matplotlib.pyplot as plt
+# df=pd.read_csv("Titanic1.csv")
+# # sns.displot(
+# #     data=df,
+# #     x="age",
+# #     kde=True,
+# #     color="blue",
+# #     col="sex"
+# # )
+# sns.displot(
+#     data=df,
+#     x="fare",
+#     color="green",
+#     col="pclass"
+# )
+# plt.title("Fare distribution")
 #
-# df = pd.read_csv("Titanic1.csv")
-#
-# sns.displot(data=df,x="age")
 # plt.show()
 
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
+from numpy.ma.core import size
+
 df=pd.read_csv("Titanic1.csv")
 # sns.displot(
 #     data=df,
@@ -30,9 +74,10 @@ df=pd.read_csv("Titanic1.csv")
 # )
 sns.displot(
     data=df,
-    x="age",
-    color="green",
-    col="sex"
+    x="fare",
+    row="sex",
+    col="pclass"
+
 )
 plt.title("Fare distribution")
 
